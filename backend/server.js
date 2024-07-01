fdcd684b7215794c719a47b0a609565e17a1ee4d
Miguel1897 <77365243+Miguel1897@users.noreply.github.com>
2024-07-01 06:13:03 -0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// src/index.js (o donde configures Express)
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -13,14 +12,14 @@ const parametrosRoutes = require('./src/routes/parametros'); // Cambia el nombre
 dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Usa el puerto de entorno proporcionado por Render
 
 app.use(cors());
 app.use(express.json());
 
 connectDB(); // Asegúrate de que esta función se conecte correctamente a tu base de datos
 
-// Rutas
+// Rutas de la API
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/medicamentos', medicamentosRoutes);
 app.use('/api/habitaciones', habitacionesRoutes);
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Asegúrate de que esta es la URL correcta de tu backend
+  baseURL: 'https://enfermeria-app-final.onrender.com/api', // Asegúrate de que esta es la URL correcta de tu backend
 });
 
 export default api;
